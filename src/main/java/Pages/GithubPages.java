b622c725d0b1a06b3296c6fbf29eebb72b7f2ae7
Admin <arksjr3099@gmail.com>
2025-01-29 18:42:14 +0300

    first version

---
+++ src/main/java/Pages/GithubPages.java
@@ -0,0 +1,10 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import static ObjectsPath.GithubPaths.*;
+
+public class GithubPages {
+
+}
---
+++ src/test/java/GithubWeb/GithubTest.java
@@ -0,0 +1,47 @@
+package GithubWeb;
+
+
+import Base.BaseClass;
+import Pages.LoginPages;
+import org.testng.annotations.Test;
+
+public class GithubTest extends BaseClass {
+
+    @Test
+    public void loginPageMethod() throws InterruptedException {
+        LoginPages login = new LoginPages(driver);
+
+        login.openGitUr();
+        login.clickOnSignInBtn();
+    }
+
+    @Test
+    public void SignUpMethod() throws InterruptedException {
+        LoginPages SignUp = new LoginPages(driver);
+        SignUp.openGitUr();
+        SignUp.clickOnSignUpBtn();
+        Thread.sleep(400);
+        SignUp.clickOnTermsBtn();
+        Thread.sleep(400);
+    }
+
+    @Test
+    public void SignUpPrivacyMethod() throws InterruptedException {
+        LoginPages SignUp = new LoginPages(driver);
+        SignUp.openGitUr();
+        SignUp.clickOnSignUpBtn();
+        Thread.sleep(300);
+        SignUp.githubPrivacy();
+        Thread.sleep(3000);
+    }
+
+    @Test
+    public void SignUpStatMethod() throws InterruptedException {
+        LoginPages SignUp = new LoginPages(driver);
+        SignUp.openGitUr();
+        SignUp.clickOnSignUpBtn();
+        Thread.sleep(300);
+        SignUp.githubStat();
+        Thread.sleep(4000);
+    }
+}
